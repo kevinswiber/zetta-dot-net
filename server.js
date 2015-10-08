cf3fcb966b0be178a03561e3eb0d29e25c9f3f59
Kevin Swiber <kswiber@gmail.com>
2015-10-08 00:30:54 -0700

    Formatting server file.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var path = require('path');
 var zetta = require('zetta');
 var DotNetScout = require('./dot_net_scout');
 
+var assemblyFile = path.join(__dirname,
+    'Zetta', 'Zetta.Example', 'bin', 'Debug', 'Zetta.Example.dll')
+
 zetta()
-  .use(DotNetScout, { assemblyFile: path.join(__dirname, 'Zetta', 'Zetta.Example', 'bin', 'Debug', 'Zetta.Example.dll') })
+  .use(DotNetScout, { assemblyFile: assemblyFile })
   .listen(process.env.PORT || 3000);
