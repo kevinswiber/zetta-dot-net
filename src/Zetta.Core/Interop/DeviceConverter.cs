7c214aaf00e5bce7ed303b69599be2f48670858b
Kevin Swiber <kswiber@gmail.com>
2015-10-29 12:31:01 -0700

    Refactoring JSON conversion factory.

---
+++ src/Zetta.Core/Interop/DeviceConverter.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zetta.Core.Interop {
+    public class DeviceConverter<T> : CustomCreationConverter<T> 
+        where T : Device {
+        public override T Create(Type objectType) {
+            return DeviceProxy.Create<T>();
+        }
+    }
+}
--- src/Zetta.Core/Interop/Serializer.cs
+++ src/Zetta.Core/Interop/Serializer.cs
@@ -7,6 +7,7 @@ using System;
 namespace Zetta.Core.Interop {
     public class Serializer {
         private static JsonSerializerSettings _settings = new JsonSerializerSettings();
+        private static IList<Type> _deserializedTypes = new List<Type>();
 
         static Serializer() {
             _settings.ContractResolver = Resolver;
@@ -17,31 +18,40 @@ namespace Zetta.Core.Interop {
             return JsonConvert.SerializeObject(device, Formatting.None, _settings);
         }
 
-        public static T Deserialize<T>(string json) where T : Device {
-            var obj = JsonConvert.DeserializeObject<T>(json, _settings);
-            return DeviceProxy.InterceptDevice(obj);
-        }
-
-        public static object Deserialize(Type type, string json) {
-            var obj = JsonConvert.DeserializeObject(json, type, _settings);
-            return DeviceProxy.InterceptDevice(type, Convert.ChangeType(obj, type));
-        }
-
         public static IEnumerable<T> DeserializeArray<T>(string json) where T : Device {
-            var t = DeviceProxy.GetInterceptedType<T>();
-            var ien = typeof(IEnumerable<>);
-            var conversionType = ien.MakeGenericType(t);
-            var deserialized = JsonConvert.DeserializeObject(json, conversionType);
-            var enumerable = (IEnumerable<T>)deserialized;
-            return enumerable.Select((d) => {
-                t.GetField("__interceptors").SetValue(d, new[] { new SetterInterceptor() });
-                return d;
-            }).ToArray().AsEnumerable();
+            EnsureTypeConverter<T>();
+
+            return JArray.Parse(json)
+                .Select((d) => JsonConvert.DeserializeObject<T>(d.ToString(), _settings))
+                .ToArray()
+                .AsEnumerable();
         }
 
         public static IEnumerable<Device> DeserializeArray(string json, Type[] types, int length) {
-            var objects = JArray.Parse(json).Children<JObject>();
-            return objects.Select((obj, index) => Deserialize(types[index], obj.ToString())).Cast<Device>().ToArray().AsEnumerable();
+            foreach (var type in types) {
+                EnsureTypeConverter(type);
+            }
+
+            return JArray.Parse(json)
+                .Select((obj, index) => JsonConvert.DeserializeObject(obj.ToString(), types[index], _settings))
+                .Cast<Device>()
+                .ToArray()
+                .AsEnumerable();
+        }
+
+        public static void EnsureTypeConverter<T>() where T : Device {
+            if (!_deserializedTypes.Contains(typeof(T))) {
+                _settings.Converters.Add(new DeviceConverter<T>());
+                _deserializedTypes.Add(typeof(T));
+            }
+        }
+
+        public static void EnsureTypeConverter(Type type) {
+            var methodInfo = typeof(Serializer).GetMethods().Where((info) => {
+                return info.Name == "EnsureTypeConverter" && info.IsGenericMethod;
+            }).First();
+
+            methodInfo.MakeGenericMethod(type).Invoke(null, null);
         }
 
         public static ZettaInteropContractResolver Resolver =
