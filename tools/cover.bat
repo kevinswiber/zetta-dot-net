1ce31bc67d0d49e62fd662c378c16dce0ed86de3
Kevin Swiber <kswiber@gmail.com>
2015-11-01 14:39:44 -0800

    Adding test coverage tooling.

---
+++ tools/cover.bat
@@ -0,0 +1,1 @@
+".\src\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -target:"C:\Program Files (x86)\NUnit.org\bin\nunit-console.exe" -targetargs:".\src\Zetta.Core.Tests\bin\Debug\Zetta.Core.Tests.dll" -register:user -filter:"+[Zetta*]* -[*Test*]*"
