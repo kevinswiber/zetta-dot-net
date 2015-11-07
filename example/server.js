4516ff3004f78c0d8466e51aa630d80c32dd48d4
Kevin Swiber <kswiber@gmail.com>
2015-11-06 21:34:59 -0800

    Preparing for NPM.

--- example/server.js
+++ example/server.js
@@ -1,8 +1,8 @@
 var path = require('path');
 var zetta = require('zetta');
-var DotNetScout = require('./dot_net_scout');
+var DotNetScout = require('../dot_net_scout');
 
-var assemblyFile = path.join(__dirname,
+var assemblyFile = path.join(__dirname, '..',
     'src', 'Zetta.Example', 'bin', 'Debug', 'Zetta.Example.dll')
 
 zetta()
