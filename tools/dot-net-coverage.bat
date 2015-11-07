dfbc07422ffbdf7580a77170f5dac39004421531
Kevin Swiber <kswiber@gmail.com>
2015-11-07 12:48:28 -0800

    Fixing path to NUnit.Console in dot-net-coverage.bat.

--- tools/dot-net-coverage.bat
+++ tools/dot-net-coverage.bat
@@ -1,4 +1,4 @@
-".\src\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -target:".\src\packages\NUnit.Console.3.0.0-rc\tools\nunit-console.exe" -targetargs:".\src\Zetta.Core.Tests\bin\Debug\Zetta.Core.Tests.dll" -register:user -filter:"+[Zetta*]* -[*Test*]*"
+".\src\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -target:".\src\packages\NUnit.Console.3.0.0-rc\tools\nunit3-console.exe" -targetargs:".\src\Zetta.Core.Tests\bin\Debug\Zetta.Core.Tests.dll" -register:user -filter:"+[Zetta*]* -[*Test*]*"
 
 ".\src\packages\ReportGenerator.2.3.2.0\tools\ReportGenerator.exe" -reports:results.xml -targetdir:.\coverage
 
