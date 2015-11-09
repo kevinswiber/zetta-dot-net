cac9d4289fab143644fefebb7cd462b79574e0bb
Kevin Swiber <kswiber@gmail.com>
2015-11-09 15:58:20 -0800

    Putting MonitorAttribute in the right namespace.

--- src/Zetta.Core/MonitorAttribute.cs
+++ src/Zetta.Core/MonitorAttribute.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Zetta {
+namespace Zetta.Core {
     [AttributeUsage(AttributeTargets.Property)]
     public class MonitorAttribute : Attribute {
         public MonitorAttribute() {
