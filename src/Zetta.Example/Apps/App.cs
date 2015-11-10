e4189f32541650edbc4c07c47e292ed23718dc97
Kevin Swiber <kswiber@gmail.com>
2015-11-09 20:57:45 -0800

    Adding Server.Observe methods to return Rx Observables.

--- src/Zetta.Example/Apps/App.cs
+++ src/Zetta.Example/Apps/App.cs
@@ -22,6 +22,66 @@ namespace Zetta.Example.Apps {
                     }
                 });
             });
+
+            /*
+            // Using Server.Observe(queries)
+
+            var queries = new[] {
+                new { type = "led" },
+                new { type = "photocell" }
+            };
+
+            server.Observe(queries)
+                .Subscribe(Observer.Create<IEnumerable<Device>>(async (devices) => {
+                    var deviceArray = devices.ToArray();
+
+                    var led = (LED)deviceArray[0];
+                    var photocell = (Photocell)deviceArray[1];
+
+                    var stream = await photocell.CreateReadStream("intensity");
+
+                    stream.Subscribe(async (obj) => {
+                        var data = (double)obj.Data;
+
+                        if (data > 1.0d && led.IsAvailable("turn-on")) {
+                            await led.Call("turn-on");
+                        } else if (data <= 1.0d && led.IsAvailable("turn-off")) {
+                            await led.Call("turn-off");
+                        }
+                    });
+                }));
+            */
+
+            /*
+            // Using Server.Observe(query) and Zip
+
+            var ledQuery = new { type = "led" };
+            var photocellQuery = new { type = "photocell" };
+
+            server.Observe<LED>(ledQuery)
+                .Zip(server.Observe<Photocell>(photocellQuery), (first, second) => {
+                    var devices = new List<Device>();
+                    devices.Add(first);
+                    devices.Add(second);
+                    return devices;
+                })
+                .Subscribe(Observer.Create<IList<Device>>(async (devices) => {
+                    var led = (LED)devices[0];
+                    var photocell = (Photocell)devices[1];
+
+                    var stream = await photocell.CreateReadStream("intensity");
+
+                    stream.Subscribe(async (obj) => {
+                        var data = (double)obj.Data;
+
+                        if (data > 1.0d && led.IsAvailable("turn-on")) {
+                            await led.Call("turn-on");
+                        } else if (data <= 1.0d && led.IsAvailable("turn-off")) {
+                            await led.Call("turn-off");
+                        }
+                    });
+                }));
+                */
         }
     }
 }
